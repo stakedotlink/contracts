334509d71511a01a58c5780998478f2d018544f6
christianagnew <christian.agnew@outlook.com>
2022-02-10 15:13:28 -0500

    updated StakingPool to work with PoolRouter

--- contracts/interfaces/IRewardsPool.sol
+++ contracts/interfaces/IRewardsPool.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC677.sol";
 
-interface IRewardsPool is IERC20 {
+interface IRewardsPool is IERC677 {
     function updateReward(address _account) external;
-
-    function withdraw() external;
-
-    function depositReward(uint256 _reward) external;
 }
--- test/utils/helpers.ts
+++ test/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat'
 import { assert } from 'chai'
+import { ERC677 } from '../../typechain-types'
 
 export const toEther = (amount: string) => {
   return ethers.utils.parseEther(amount).toHexString()
@@ -21,4 +22,16 @@ export const assertThrowsAsync = async (fn: Function, regExp: string) => {
 export const deploy = async (contractName: string, args: Array<any>) => {
   const Contract = await ethers.getContractFactory(contractName)
   return Contract.deploy(...args)
+}
+
+export const setupAccounts = async (token: ERC677) => {
+  const signers = await ethers.getSigners()
+  const accounts = await Promise.all(
+    signers.map(async (signer, index) => {
+      let account = await signer.getAddress()
+      await token.transfer(account, toEther(index < 4 ? '10000' : '0'))
+      return account
+    })
+  )
+  return { signers, accounts }
 }
