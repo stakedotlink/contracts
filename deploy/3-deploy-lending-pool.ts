5bbcbd03348f9457553c88230535402006534b1c
christianagnew <christian.agnew@outlook.com>
2022-11-08 22:02:54 -0500

    updated deployment scripts

--- deploy/3-deploy-lending-pool.ts
+++ deploy/3-deploy-lending-pool.ts
@@ -27,6 +27,10 @@ module.exports = async function (hre: HardhatRuntimeEnvironment) {
       LendingPool.rateConstantE,
     ],
   })
+  const lendingPool = await ethers.getContract('LendingPool')
+
+  const tx = await poolRouter.setLendingPool(lendingPool.address)
+  await tx.wait()
 }
 
 module.exports.tags = ['LendingPool']
