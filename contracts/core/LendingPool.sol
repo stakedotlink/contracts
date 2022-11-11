5bf2796e78f1c452a88c8765384c683c36b7d9fd
Jonny <jonny@smartcontract.com>
2022-11-11 19:43:24 +0000

    Ability to lock & vest allowance tokens in the lending pool

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -23,6 +23,13 @@ contract LendingPool is RewardsPoolController {
     event AllowanceStaked(address indexed user, uint amount);
     event AllowanceWithdrawn(address indexed user, uint amount);
 
+    struct VestingSchedule {
+        uint totalAmount;
+        uint64 startTimestamp;
+        uint64 durationSeconds;
+    }
+    mapping(address => VestingSchedule) private vestingSchedules;
+
     constructor(
         address _allowanceToken,
         string memory _dTokenName,
@@ -52,9 +59,54 @@ contract LendingPool is RewardsPoolController {
 
         if (msg.sender == address(allowanceToken)) {
             _stakeAllowance(_sender, _value);
+
+            if (_calldata.length > 1) {
+                (uint64 startTimestamp, uint64 durationSeconds) = abi.decode(_calldata, (uint64, uint64));
+                _setVestingSchedule(_sender, _value, startTimestamp, durationSeconds);
+            }
         } else {
             distributeToken(msg.sender);
         }
+    }
+
+    /**
+     * @notice returns an accounts balance minus the amount of tokens that are currently vesting
+     * @param _account account
+     * @return balance accounts balance
+     */
+    function balanceOf(address _account) public view virtual override(ERC20, IERC20) returns (uint256) {
+        return super.balanceOf(_account) - (vestingSchedules[_account].totalAmount - _vestedTokens(_account));
+    }
+
+    /**
+     * @notice deletes the vesting schedule if the transfer occurs after vesting ends
+     * @param _from account sending from
+     * @param _to account sending to
+     * @param _amount amount being sent
+     */
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal virtual override {
+        require(balanceOf(_from) >= _amount, "ERC20: transfer amount exceeds balance");
+
+        VestingSchedule storage vestingSchedule = vestingSchedules[_from];
+        if (block.timestamp > vestingSchedule.startTimestamp + vestingSchedule.durationSeconds) {
+            delete vestingSchedules[_from];
+        }
+
+        super._transfer(_from, _to, _amount);
+    }
+
+    /**
+     * @notice returns an account's staked amount for use by reward pools
+     * controlled by this contract. Overridden as the the staked amount needs to include any vesting tokens.
+     * @dev required by RewardsPoolController
+     * @return account's staked amount
+     */
+    function staked(address _account) external view override returns (uint) {
+        return (rewardRedirects[_account] == address(0) ? super.balanceOf(_account) : 0) + redirectedStakes[_account];
     }
 
     /**
@@ -83,6 +135,7 @@ contract LendingPool is RewardsPoolController {
      **/
     function withdrawAllowance(uint _amount) external updateRewards(msg.sender) {
         require(!poolRouter.isReservedMode(), "Allowance cannot be withdrawn when pools are reserved");
+        require(balanceOf(msg.sender) >= _amount, "Withdrawal amount exceeds balance");
 
         uint toWithdraw = _amount;
         if (_amount == type(uint).max) {
@@ -93,6 +146,15 @@ contract LendingPool is RewardsPoolController {
         allowanceToken.safeTransfer(msg.sender, toWithdraw);
 
         emit AllowanceWithdrawn(msg.sender, toWithdraw);
+    }
+
+    /**
+     * @notice returns the vesting schedule of a given account
+     * @param _account account
+     * @return vestingSchedule accounts vesting schedule
+     */
+    function getVestingSchedule(address _account) external view returns (VestingSchedule memory) {
+        return vestingSchedules[_account];
     }
 
     /**
@@ -123,5 +185,49 @@ contract LendingPool is RewardsPoolController {
     function _stakeAllowance(address _sender, uint _amount) private updateRewards(_sender) {
         _mint(_sender, _amount);
         emit AllowanceStaked(_sender, _amount);
+    }
+
+    /**
+     * @notice sets an accounts derivative token vesting schedule. If a schedule already exists:
+     * - If the new start timestamp is after the previous schedule, the schedule is overwritten and any remaining vesting tokens go into the new schedule
+     * - Will release any tokens that have vested but not transferred
+     * - If the start timestamp is before the current schedule, the current schedule is used
+     */
+    function _setVestingSchedule(
+        address _account,
+        uint _amount,
+        uint64 _startTimestamp,
+        uint64 _durationSeconds
+    ) internal {
+        require(_startTimestamp > 0, "Start timestamp cannot be 0");
+        require(_durationSeconds > 0, "Seconds duration cannot be 0");
+
+        VestingSchedule storage vestingSchedule = vestingSchedules[_account];
+        if (_startTimestamp > vestingSchedule.startTimestamp) {
+            if (vestingSchedule.startTimestamp != 0) {
+                vestingSchedule.totalAmount -= _vestedTokens(_account);
+            }
+            vestingSchedule.startTimestamp = _startTimestamp;
+            vestingSchedule.durationSeconds = _durationSeconds;
+        }
+        vestingSchedule.totalAmount += _amount;
+    }
+
+    /**
+     * @notice Returns the amount of tokens that are currently locked within vesting
+     */
+    function _vestedTokens(address _account) internal view virtual returns (uint256) {
+        VestingSchedule memory vestingSchedule = vestingSchedules[_account];
+        uint totalVested = vestingSchedule.totalAmount;
+        uint64 startTimestamp = vestingSchedule.startTimestamp;
+        uint64 timestamp = uint64(block.timestamp);
+
+        if (totalVested == 0 || timestamp < startTimestamp) {
+            return 0;
+        } else if (timestamp > startTimestamp + vestingSchedule.durationSeconds) {
+            return totalVested;
+        } else {
+            return ((totalVested * (timestamp - startTimestamp)) / vestingSchedule.durationSeconds);
+        }
     }
 }
--- test/core/lending-pool.test.ts
+++ test/core/lending-pool.test.ts
@@ -9,6 +9,8 @@ import {
   RampUpCurve,
 } from '../../typechain-types'
 import { assert, expect } from 'chai'
+import { defaultAbiCoder } from 'ethers/lib/utils'
+import { time } from '@nomicfoundation/hardhat-network-helpers'
 
 describe('LendingPool', () => {
   let token: ERC677
@@ -346,5 +348,170 @@ describe('LendingPool', () => {
       2516,
       'current rate is wrong'
     )
+  })
+
+  describe.only('token vesting', async () => {
+    let cliff: number
+    let duration: number
+
+    beforeEach(async () => {
+      cliff = (await time.latest()) + 3600
+      duration = 3600
+      await allowanceToken.transferAndCall(
+        lendingPool.address,
+        toEther(1000),
+        defaultAbiCoder.encode(['uint64', 'uint64'], [cliff, duration])
+      )
+    })
+
+    it('should see token vest', async () => {
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance should be zero')
+    })
+
+    it('should not be able to transfer vested tokens', async () => {
+      await expect(lendingPool.transfer(accounts[1], toEther(500))).to.be.revertedWith(
+        'ERC20: transfer amount exceeds balance'
+      )
+    })
+
+    it('should not be able to withdraw vested tokens', async () => {
+      await expect(lendingPool.withdrawAllowance(toEther(1000))).to.be.revertedWith(
+        'Withdrawal amount exceeds balance'
+      )
+    })
+
+    it('should see amount staked include vesting', async () => {
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        1000,
+        'staked balance incorrect'
+      )
+    })
+
+    it('should see able to stake and transfer more tokens without vest', async () => {
+      await allowanceToken.transferAndCall(lendingPool.address, toEther(1000), '0x00')
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        1000,
+        'balance does not match'
+      )
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        2000,
+        'staked balance incorrect'
+      )
+      await lendingPool.transfer(accounts[1], toEther(1000))
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance does not match')
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        1000,
+        'staked balance incorrect'
+      )
+    })
+
+    it('should see zero balance after cliff ends', async () => {
+      await time.increaseTo(cliff)
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance should be zero')
+    })
+
+    it('should be able to get 50% of balance during vest', async () => {
+      await time.increaseTo(cliff + 1800)
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        500,
+        'balance should be zero'
+      )
+    })
+
+    it('should be able transfer 50% of vested balance', async () => {
+      await time.setNextBlockTimestamp(cliff + 1800)
+
+      await lendingPool.transfer(accounts[1], toEther(500))
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance should be zero')
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        500,
+        'staked balance incorrect'
+      )
+    })
+
+    it('should be able transfer 50% and then 75% of vested balance', async () => {
+      await time.setNextBlockTimestamp(cliff + 1800)
+      await lendingPool.transfer(accounts[1], toEther(500))
+
+      await time.setNextBlockTimestamp(cliff + 2700)
+      await lendingPool.transfer(accounts[1], toEther(250))
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance should be zero')
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        250,
+        'staked balance incorrect'
+      )
+    })
+
+    it('should be able to transfer all once vested', async () => {
+      await time.setNextBlockTimestamp(cliff + 3600)
+      await lendingPool.transfer(accounts[1], toEther(1000))
+
+      assert.equal(fromEther(await lendingPool.balanceOf(accounts[0])), 0, 'balance should be zero')
+      assert.equal(fromEther(await lendingPool.staked(accounts[0])), 0, 'staked balance incorrect')
+    })
+
+    it('should be able to vest more tokens on a new schedule', async () => {
+      await time.setNextBlockTimestamp(cliff + 1800)
+
+      let cliff2 = (await time.latest()) + 7200
+      await allowanceToken.transferAndCall(
+        lendingPool.address,
+        toEther(1000),
+        defaultAbiCoder.encode(['uint64', 'uint64'], [cliff2, duration])
+      )
+
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        500,
+        'balance does not match'
+      )
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        2000,
+        'staked balance incorrect'
+      )
+
+      await time.increaseTo(cliff2 + 1800)
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        1250,
+        'balance does not match'
+      )
+    })
+
+    it('should be able to vest more tokens on the same schedule', async () => {
+      await time.setNextBlockTimestamp(cliff + 1800)
+
+      await allowanceToken.transferAndCall(
+        lendingPool.address,
+        toEther(1000),
+        defaultAbiCoder.encode(['uint64', 'uint64'], [cliff, duration])
+      )
+
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        1000,
+        'balance does not match'
+      )
+      assert.equal(
+        fromEther(await lendingPool.staked(accounts[0])),
+        2000,
+        'staked balance incorrect'
+      )
+
+      await time.increaseTo(cliff + 3600)
+      assert.equal(
+        fromEther(await lendingPool.balanceOf(accounts[0])),
+        2000,
+        'balance does not match'
+      )
+    })
   })
 })
