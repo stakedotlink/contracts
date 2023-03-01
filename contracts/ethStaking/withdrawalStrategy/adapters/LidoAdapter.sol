698742f1b65c926de7537b924d3c7b218b4327b2
BkChoy <christian.agnew@outlook.com>
2023-03-01 18:20:17 -0500

    lido withdrawal adapter

---
+++ contracts/ethStaking/withdrawalStrategy/adapters/LidoAdapter.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+import "../base/WithdrawalAdapter.sol";
+import "../interfaces/ILidoWQERC721.sol";
+
+/**
+ * @title Lido Adapter
+ * @notice Withdrawal adapter for Lido
+ */
+contract LidoAdapter is WithdrawalAdapter {
+    struct Withdrawal {
+        uint256 totalETHAmount;
+        uint256 withdrawnETHAmount;
+        address owner;
+    }
+
+    ILidoWQERC721 public wqERC721;
+    mapping(uint256 => Withdrawal) public withdrawals;
+
+    uint256 private totalOutstandingDeposits;
+
+    event InitiateWithdrawal(
+        address indexed owner,
+        uint256 indexed requestId,
+        uint256 withdrawalAmount,
+        uint256 totalAmount
+    );
+    event FinalizeWithdrawal(
+        address indexed owner,
+        uint256 indexed requestId,
+        uint256 withdrawalAmount,
+        uint256 totalFinalizedAmount
+    );
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address _controller,
+        address _wqERC721,
+        uint256 _instantAmountBasisPoints
+    ) public initializer {
+        __WithdrawalAdapter_init(_controller, _instantAmountBasisPoints);
+        wqERC721 = ILidoWQERC721(_wqERC721);
+    }
+
+    function getTotalDeposits() external view override returns (uint256) {
+        return totalOutstandingDeposits;
+    }
+
+    function getRequestIds() external view returns (uint256[] memory) {
+        return wqERC721.getWithdrawalRequests(address(this));
+    }
+
+    function getWithdrawableEther() external view returns (uint256[] memory) {
+        uint256[] memory requestIds = wqERC721.getWithdrawalRequests(address(this));
+        uint256[] memory hints = wqERC721.findCheckpointHintsUnbounded(requestIds);
+        return wqERC721.getClaimableEther(requestIds, hints);
+    }
+
+    function initiateWithdrawal(uint256 _requestId) external {
+        uint256[] memory reqList = new uint256[](1);
+        reqList[0] = _requestId;
+        ILidoWQERC721.WithdrawalRequestStatus memory requestStatus = wqERC721.getWithdrawalStatus(reqList)[0];
+
+        uint256 totalAmount = requestStatus.amountOfStETH;
+        uint256 instantWithdrawalAmount = (totalAmount * instantAmountBasisPoints) / 10000;
+
+        if (instantWithdrawalAmount > controller.availableDeposits()) revert InsufficientFundsForWithdrawal();
+
+        withdrawals[_requestId] = Withdrawal(totalAmount, instantWithdrawalAmount, msg.sender);
+        totalOutstandingDeposits += instantWithdrawalAmount;
+        wqERC721.transferFrom(msg.sender, address(this), _requestId);
+        controller.adapterWithdraw(msg.sender, instantWithdrawalAmount);
+
+        emit InitiateWithdrawal(msg.sender, _requestId, instantWithdrawalAmount, totalAmount);
+    }
+
+    function finalizeWithdrawals(uint256[] calldata _requestIds, uint256[] calldata _hints) external {
+        uint256 startingBalance = address(this).balance;
+        uint256[] memory claimableEther = wqERC721.getClaimableEther(_requestIds, _hints);
+
+        wqERC721.claimWithdrawals(_requestIds, _hints);
+
+        uint256 totalFinalizedDeposits;
+        for (uint i = 0; i < _requestIds.length; ++i) {
+            uint256 claimableETH = claimableEther[i];
+            Withdrawal memory withdrawal = withdrawals[_requestIds[i]];
+
+            uint256 toRemainInPool = (claimableETH * feeBasisPoints) / 10000;
+            if (claimableETH < withdrawal.totalETHAmount) {
+                toRemainInPool += withdrawal.totalETHAmount - claimableETH;
+            }
+
+            uint256 toWithdraw;
+            if (toRemainInPool < claimableETH) {
+                toWithdraw = claimableETH - toRemainInPool;
+                _sendEther(withdrawal.owner, toWithdraw);
+            }
+
+            totalFinalizedDeposits += withdrawal.withdrawnETHAmount;
+            emit FinalizeWithdrawal(withdrawal.owner, _requestIds[i], toWithdraw, claimableETH);
+        }
+
+        totalOutstandingDeposits -= totalFinalizedDeposits;
+        controller.adapterDeposit{value: address(this).balance - startingBalance}();
+    }
+}
---
+++ contracts/ethStaking/withdrawalStrategy/interfaces/ILidoWQERC721.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+interface ILidoWQERC721 {
+    struct WithdrawalRequestStatus {
+        /// @notice stETH token amount that was locked on withdrawal queue for this request
+        uint256 amountOfStETH;
+        /// @notice amount of stETH shares locked on withdrawal queue for this request
+        uint256 amountOfShares;
+        /// @notice address that can claim or transfer this request
+        address owner;
+        /// @notice timestamp of when the request was created, in seconds
+        uint256 timestamp;
+        /// @notice true, if request is finalized
+        bool isFinalized;
+        /// @notice true, if request is claimed. Request is claimable if (isFinalized && !isClaimed)
+        bool isClaimed;
+    }
+
+    // @notice Returns statuses for the array of request ids
+    /// @param _requestIds array of withdrawal request ids
+    function getWithdrawalStatus(uint256[] calldata _requestIds)
+        external
+        view
+        returns (WithdrawalRequestStatus[] memory statuses);
+
+    /// @notice Returns all withdrawal requests that belongs to the `_owner` address
+    function getWithdrawalRequests(address _owner) external view returns (uint256[] memory requestsIds);
+
+    /// @notice Returns array of claimable eth amounts that is locked for each request
+    /// @param _requestIds array of request ids to find claimable ether for
+    /// @param _hints checkpoint hint for each id.
+    ///   Can be retrieved with `findCheckpointHints()` or `findCheckpointHintsUnbounded()`
+    function getClaimableEther(uint256[] calldata _requestIds, uint256[] calldata _hints)
+        external
+        view
+        returns (uint256[] memory claimableEthValues);
+
+    /// @notice Claim a batch of withdrawal requests once finalized (claimable) sending locked ether to the owner
+    /// @param _requestIds array of request ids to claim
+    /// @param _hints checkpoint hint for each id.
+    ///   Can be retrieved with `findCheckpointHints()` or `findCheckpointHintsUnbounded()`
+    /// @dev
+    ///  Reverts if any requestId or hint in arguments are not valid
+    ///  Reverts if any request is not finalized or already claimed
+    ///  Reverts if msg sender is not an owner of the requests
+    function claimWithdrawals(uint256[] calldata _requestIds, uint256[] calldata _hints) external;
+
+    /// @notice Finds the list of hints for the given `_requestIds` searching among the checkpoints with indices
+    ///  in the range `[1, lastCheckpointIndex]`. NB! Array of request ids should be sorted
+    /// @dev WARNING! OOG is possible if used onchain.
+    /// @param _requestIds ids of the requests sorted in the ascending order to get hints for
+    function findCheckpointHintsUnbounded(uint256[] calldata _requestIds) external view returns (uint256[] memory hintIds);
+
+    /// @dev See {IERC721-transferFrom}.
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _requestId
+    ) external;
+}
