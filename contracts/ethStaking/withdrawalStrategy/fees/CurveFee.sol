e7810a8bedee3c46420ce597d8c4268b5ba4251e
BkChoy <christian.agnew@outlook.com>
2023-03-15 18:12:50 -0400

    added max fee to curvefee

--- contracts/ethStaking/withdrawalStrategy/fees/CurveFee.sol
+++ contracts/ethStaking/withdrawalStrategy/fees/CurveFee.sol
@@ -18,12 +18,15 @@ contract CurveFee is IFeeAdapter, Ownable {
     int128 public toIndex;
 
     uint256 public minFeeBasisPoints;
+    uint256 public maxFeeBasisPoints;
     uint256 public feeUndercutBasisPoints;
 
     event SetMinFeeBasisPoints(uint256 minFeeBasisPoints);
+    event SetMaxFeeBasisPoints(uint256 maxFeeBasisPoints);
     event SetFeeUndercutBasisPoints(uint256 feeUndercutBasisPoints);
 
     error InvalidMinFeeBasisPoints();
+    error InvalidMaxFeeBasisPoints();
     error InvalidFeeUndercutBasisPoints();
 
     constructor(
@@ -31,12 +34,14 @@ contract CurveFee is IFeeAdapter, Ownable {
         int128 _fromIndex,
         int128 _toIndex,
         uint256 _minFeeBasisPoints,
+        uint256 _maxFeeBasisPoints,
         uint256 _feeUndercutBasisPoints
     ) {
         curvePool = ICurvePool(_curvePool);
         fromIndex = _fromIndex;
         toIndex = _toIndex;
         setMinFeeBasisPoints(_minFeeBasisPoints);
+        setMaxFeeBasisPoints(_maxFeeBasisPoints);
         setFeeUndercutBasisPoints(_feeUndercutBasisPoints);
     }
 
@@ -49,6 +54,7 @@ contract CurveFee is IFeeAdapter, Ownable {
     function getFee(uint256 _lsdAmountToSwap, uint256 _underlyingValue) external view returns (uint256) {
         uint256 amountReceived = curvePool.get_dy(fromIndex, toIndex, _lsdAmountToSwap);
         uint256 minFee = (_underlyingValue * minFeeBasisPoints) / BASIS_POINTS;
+        uint256 maxFee = (_underlyingValue * maxFeeBasisPoints) / BASIS_POINTS;
         uint256 fee;
 
         if (amountReceived < _underlyingValue) {
@@ -57,6 +63,8 @@ contract CurveFee is IFeeAdapter, Ownable {
 
         if (fee < minFee) {
             return minFee;
+        } else if (fee > maxFee) {
+            return maxFee;
         } else {
             return fee;
         }
@@ -70,6 +78,16 @@ contract CurveFee is IFeeAdapter, Ownable {
         if (_minFeeBasisPoints > 500) revert InvalidMinFeeBasisPoints();
         minFeeBasisPoints = _minFeeBasisPoints;
         emit SetMinFeeBasisPoints(_minFeeBasisPoints);
+    }
+
+    /**
+     * @notice sets the maximum fee basis point fee to be paid on withdrawals
+     * @param _maxFeeBasisPoints maximum basis point fee
+     */
+    function setMaxFeeBasisPoints(uint256 _maxFeeBasisPoints) public onlyOwner {
+        if (_maxFeeBasisPoints < 1000) revert InvalidMaxFeeBasisPoints();
+        maxFeeBasisPoints = _maxFeeBasisPoints;
+        emit SetMaxFeeBasisPoints(_maxFeeBasisPoints);
     }
 
     /**
--- test/ethStaking/withdrawalStrategy/curve-fee.test.ts
+++ test/ethStaking/withdrawalStrategy/curve-fee.test.ts
@@ -8,13 +8,17 @@ describe('CurveFee', () => {
 
   beforeEach(async () => {
     curvePool = (await deploy('CurvePoolMock', [toEther(5)])) as CurvePoolMock
-    curveFee = (await deploy('CurveFee', [curvePool.address, 0, 0, 100, 1000])) as CurveFee
+    curveFee = (await deploy('CurveFee', [curvePool.address, 0, 0, 100, 3000, 1000])) as CurveFee
   })
 
   it('getFee should work correctly', async () => {
     assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(6))), 0.9)
-    assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(8))), 2.7)
+    assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(7))), 1.8)
+
     assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(5))), 0.05)
     assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(4))), 0.04)
+
+    assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(8))), 2.4)
+    assert.equal(fromEther(await curveFee.getFee(toEther(6), toEther(10))), 3)
   })
 })
