4d85b16aee8f01e85ac4e0f06d2e60321484fdc0
jongregis <jon.gregis@gmail.com>
2023-04-26 14:27:49 -0500

    add new adapters

---
+++ contracts/liquidSDIndex/adapters/StakewiseLSDIndexAdapter.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+import "../base/LSDIndexAdapter.sol";
+
+/**
+ * @title Stakewise sETH2 Adapter
+ * @notice Adapter for Stakewise's sETH2
+ */
+contract StakewiseLSDIndexAdapter is LSDIndexAdapter {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _token, address _indexPool) public initializer {
+        __LiquidSDAdapter_init(_token, _indexPool);
+    }
+
+    /**
+     * @notice returns the exchange rate between the underlying asset and this adapter's token
+     * @return exchange rate
+     */
+    function getExchangeRate() public view override returns (uint256) {
+        return 1 ether;
+    }
+}
