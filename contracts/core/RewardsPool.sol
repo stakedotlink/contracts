253cbd18ac5f0bc881516a175846803f29d90d4d
BkChoy <christian.agnew@outlook.com>
2023-05-24 16:07:34 -0400

    removed token logic from RewardsPoolController

--- contracts/core/RewardsPool.sol
+++ contracts/core/RewardsPool.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/IRewardsPoolController.sol";
+import "./interfaces/IERC677.sol";
 
 /**
  * @title RewardsPool
