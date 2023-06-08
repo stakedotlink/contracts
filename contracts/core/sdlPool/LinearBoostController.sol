16e8cd079ed3f053626f027a4b7014981075da1c
BkChoy <christian.agnew@outlook.com>
2023-06-08 15:22:22 -0400

    added missing natspec comments

--- contracts/core/sdlPool/LinearBoostController.sol
+++ contracts/core/sdlPool/LinearBoostController.sol
@@ -21,16 +21,32 @@ contract LinearBoostController is Ownable {
         maxBoost = _maxBoost;
     }
 
+    /**
+     * @notice returns the amount of boost balance received for a certain amount and locking duration
+     * @param _amount amount of tokens to lock
+     * @param _lockingDuration duration of the locking period
+     * @return amounnt of boost balance received in addition to the unboosted balance
+     */
     function getBoostAmount(uint256 _amount, uint64 _lockingDuration) external view returns (uint256) {
         if (_lockingDuration > maxLockingDuration) revert MaxLockingDurationExceeded();
         return (_amount * uint256(maxBoost) * uint256(_lockingDuration)) / uint256(maxLockingDuration);
     }
 
+    /**
+     * @notice sets the maximum locking duration
+     * @param _maxLockingDuration max locking duration in seconds
+     */
     function setMaxLockingDuration(uint64 _maxLockingDuration) external onlyOwner {
         maxLockingDuration = _maxLockingDuration;
         emit SetMaxLockingDuration(_maxLockingDuration);
     }
 
+    /**
+     * @notice sets the maximum boost multiplier
+     * @dev a multiplier of 1 would mean that a staker's balance is doubled if they lock for the max
+     * locking duration
+     * @param _maxBoost max boost multiplier
+     */
     function setMaxBoost(uint64 _maxBoost) external onlyOwner {
         maxBoost = _maxBoost;
         emit SetMaxBoost(_maxBoost);
