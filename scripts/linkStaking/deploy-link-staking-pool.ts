75ab9bafe8af94b8edf838b4876a4f2e664478df
BkChoy <christian.agnew@outlook.com>
2023-06-14 16:40:51 -0400

    deploy sdl pool for testing

--- scripts/linkStaking/deploy-link-staking-pool.ts
+++ scripts/linkStaking/deploy-link-staking-pool.ts
@@ -1,5 +1,6 @@
-import { DelegatorPool, ERC677, PoolRouter } from '../../typechain-types'
+import { ERC677, SDLPool } from '../../typechain-types'
 import { updateDeployments, deploy, getContract, deployUpgradeable } from '../utils/deployment'
+import { getAccounts } from '../utils/helpers'
 
 // LINK Wrapped Staking Derivative Token
 const LINK_WrappedSDToken = {
@@ -14,17 +15,16 @@ const LINK_StakingPool = {
 }
 
 async function main() {
+  const { accounts } = await getAccounts()
   const linkToken = (await getContract('LINKToken')) as ERC677
-  const delegatorPool = (await getContract('DelegatorPool')) as DelegatorPool
-  const poolRouter = (await getContract('PoolRouter')) as PoolRouter
+  const sdlPool = (await getContract('SDLPool')) as SDLPool
 
   const stakingPool = await deployUpgradeable('StakingPool', [
     linkToken.address,
     LINK_StakingPool.derivativeTokenName,
     LINK_StakingPool.derivativeTokenSymbol,
     LINK_StakingPool.fees,
-    poolRouter.address,
-    delegatorPool.address,
+    accounts[0],
   ])
   console.log('LINK_StakingPool deployed: ', stakingPool.address)
 
@@ -35,32 +35,26 @@ async function main() {
   ])
   console.log('LINK_WrappedSDToken token deployed: ', wsdToken.address)
 
-  const stLinkDelegatorRewardsPool = await deploy('RewardsPoolWSD', [
-    delegatorPool.address,
+  const stLinkSDLRewardsPool = await deploy('RewardsPoolWSD', [
+    sdlPool.address,
     stakingPool.address,
     wsdToken.address,
   ])
-  console.log('stLINK_DelegatorRewardsPool deployed: ', stLinkDelegatorRewardsPool.address)
+  console.log('stLINK_SDLRewardsPool deployed: ', stLinkSDLRewardsPool.address)
 
-  let tx = await poolRouter.addPool(stakingPool.address, 0, true)
-  await tx.wait()
-
-  tx = await delegatorPool.addToken(stakingPool.address, stLinkDelegatorRewardsPool.address)
-  await tx.wait()
-
-  tx = await delegatorPool.setPoolRouter(poolRouter.address)
+  let tx = await sdlPool.addToken(stakingPool.address, stLinkSDLRewardsPool.address)
   await tx.wait()
 
   updateDeployments(
     {
       LINK_StakingPool: stakingPool.address,
       LINK_WrappedSDToken: wsdToken.address,
-      stLINK_DelegatorRewardsPool: stLinkDelegatorRewardsPool.address,
+      stLINK_SDLRewardsPool: stLinkSDLRewardsPool.address,
     },
     {
       LINK_StakingPool: 'StakingPool',
       LINK_WrappedSDToken: 'WrappedSDToken',
-      stLINK_DelegatorRewardsPool: 'RewardsPoolWSD',
+      stLINK_SDLRewardsPool: 'RewardsPoolWSD',
     }
   )
 }
