87de9b803a4ceaeac2597e1dca084fb844fdf282
Bartosz Wojciechowski <zly.ojciec@gmail.com>
2023-08-03 13:50:07 +0200

    prettier fix

--- scripts/test/setup-test-env-migrations.ts
+++ scripts/test/setup-test-env-migrations.ts
@@ -1,13 +1,6 @@
 import { getAccounts, toEther } from '../utils/helpers'
-import { getContract, 
-   deploy, updateDeployments
- } from '../utils/deployment'
-import {
-  ERC677,
-  LPLMigration,
-  SDLPool,
-  StakingAllowance,
-} from '../../typechain-types'
+import { getContract, deploy, updateDeployments } from '../utils/deployment'
+import { ERC677, LPLMigration, SDLPool, StakingAllowance } from '../../typechain-types'
 import { ethers } from 'hardhat'
 
 async function main() {
@@ -18,7 +11,7 @@ async function main() {
   const poolOwnersV1 = (await getContract('PoolOwnersV1')) as any
   const ownersRewardsPoolV1 = (await getContract('LINK_OwnersRewardsPoolV1')) as any
   const linkToken = (await getContract('LINKToken')) as ERC677
-  const delegatorPool = (await getContract('DelegatorPool')) as any 
+  const delegatorPool = (await getContract('DelegatorPool')) as any
   const LINK_StakingPool = (await getContract('LINK_StakingPool')) as any
   const LINK_StakingQueue = (await getContract('LINK_StakingQueue')) as any
   const sdlPool = (await getContract('SDLPool')) as SDLPool
@@ -36,16 +29,22 @@ async function main() {
   await sdlToken.mint(accounts[2], toEther(100000))
   await sdlToken.connect(signers[2]).transferAndCall(delegatorPool.address, toEther(90000), '0x')
 
-  let stLINK_DelegatorRewardsPool = (await deploy('RewardsPool', [
+  let stLINK_DelegatorRewardsPool = await deploy('RewardsPool', [
     delegatorPool.address,
     LINK_StakingPool.address,
-  ])) 
-  await delegatorPool.addToken(LINK_StakingPool.address, stLINK_DelegatorRewardsPool.address)        
-  
+  ])
+  await delegatorPool.addToken(LINK_StakingPool.address, stLINK_DelegatorRewardsPool.address)
+
   // stake LINK in LINK POOL
-  
+
   await linkToken.transfer(accounts[3], toEther(100))
-  await linkToken.connect(signers[3]).transferAndCall(LINK_StakingQueue.address, toEther(100), ethers.utils.defaultAbiCoder.encode(['bool'], [false]))
+  await linkToken
+    .connect(signers[3])
+    .transferAndCall(
+      LINK_StakingQueue.address,
+      toEther(100),
+      ethers.utils.defaultAbiCoder.encode(['bool'], [false])
+    )
 
   // send LINK rewards to LINK POOL
 
@@ -59,11 +58,9 @@ async function main() {
 
   await delegatorPool.retireDelegatorPool([], sdlPool.address)
 
-  updateDeployments(
-    {
-      stLINK_DelegatorRewardsPool: stLINK_DelegatorRewardsPool.address,
-    }
-  )
+  updateDeployments({
+    stLINK_DelegatorRewardsPool: stLINK_DelegatorRewardsPool.address,
+  })
 }
 
 main()
