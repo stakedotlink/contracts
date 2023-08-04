e3f7c6108a957db8924c83ea57828d7e5b4fad28
Bartosz Wojciechowski <zly.ojciec@gmail.com>
2023-08-04 11:51:30 +0200

    Update mock scripts

---
+++ scripts/test/start-test-envs.js
@@ -0,0 +1,89 @@
+const child_process = require('child_process')
+
+// Start hardhat, wait for `deploy-status-ready` status, then run mock data script
+
+async function compile() {
+  const child = child_process.spawn('npx', ['yarn', 'compile'])
+
+  child.stdout.on('data', (data) => {
+    console.log('yarn compile - process (' + child.pid + '): ' + data)
+  })
+
+  return await new Promise((resolve) => {
+    child.on('close', () => {
+      resolve('success')
+    })
+  })
+}
+
+async function startHardhat() {
+  const child = child_process.spawn('npx', ['yarn', 'start'])
+
+  child.stdout.on('data', (data) => {
+    console.log('yarn compile - process (' + child.pid + '): ' + data)
+  })
+
+  return await new Promise((resolve) => {
+    child.stdout.on('data', (data) => {
+      if (data.toString().includes('Mined empty block')) {
+        resolve('success')
+      }
+    })
+  })
+}
+
+async function deploy() {
+  const child = child_process.spawn('npx', ['yarn', 'deploy'])
+
+  child.stdout.on('data', (data) => {
+    console.log('yarn deploy - process (' + child.pid + '): ' + data)
+  })
+
+  return await new Promise((resolve) => {
+    child.on('close', () => {
+      resolve('success')
+    })
+  })
+}
+
+async function testnEnv() {
+  const child = child_process.spawn('npx', ['yarn', 'setup-test-env'])
+
+  child.stdout.on('data', (data) => {
+    console.log('yarn setup-test-env - process (' + child.pid + '): ' + data)
+  })
+
+  return await new Promise((resolve) => {
+    child.on('close', () => {
+      resolve('success')
+    })
+  })
+}
+
+async function testnEnvMigration() {
+  const child = child_process.spawn('npx', ['yarn', 'setup-test-env-migrations'])
+
+  child.stdout.on('data', (data) => {
+    console.log('yarn setup-test-env-migrations - process (' + child.pid + '): ' + data)
+  })
+
+  return await new Promise((resolve) => {
+    child.on('close', () => {
+      resolve('success')
+    })
+  })
+}
+
+async function run() {
+  await compile()
+  await startHardhat()
+  await deploy()
+  await testnEnv()
+  await testnEnvMigration()
+}
+
+run().catch((error) => {
+  console.log('error', error)
+  console.error(error)
+  process.exit(1)
+})
