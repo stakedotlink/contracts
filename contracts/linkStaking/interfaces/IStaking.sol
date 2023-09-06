89b9b74af60bfe4b5366677074eb43169f0a92f7
BkChoy <christian.agnew@outlook.com>
2023-09-06 12:24:08 -0400

    update vaults to support link staking 0.2

--- contracts/linkStaking/interfaces/IStaking.sol
+++ contracts/linkStaking/interfaces/IStaking.sol
@@ -2,29 +2,13 @@
 pragma solidity 0.8.15;
 
 interface IStaking {
-    function getCommunityStakerLimits() external view returns (uint256, uint256);
-
-    function getOperatorLimits() external view returns (uint256, uint256);
+    function getStakerPrincipal(address _staker) external view returns (uint256);
 
     function getMaxPoolSize() external view returns (uint256);
 
-    function getTotalStakedAmount() external view returns (uint256);
+    function getTotalPrincipal() external view returns (uint256);
 
-    function isActive() external view returns (bool);
-
-    function isOperator(address staker) external view returns (bool);
-
-    function getStake(address staker) external view returns (uint256);
+    function getStakerLimits() external view returns (uint256, uint256);
 
     function migrate(bytes calldata data) external;
-
-    function getBaseReward(address staker) external view returns (uint256);
-
-    function getDelegationReward(address staker) external view returns (uint256);
-
-    function getMigrationTarget() external view returns (address);
-
-    function isPaused() external view returns (bool);
-
-    function raiseAlert() external;
 }
