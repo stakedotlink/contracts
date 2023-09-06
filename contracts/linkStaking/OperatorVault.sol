a74b1f537df3343c0134c9c21c4ee6c64b0adf4e
BkChoy <christian.agnew@outlook.com>
2023-09-06 12:24:08 -0400

    update vaults to support link staking 0.2

--- contracts/linkStaking/OperatorVault.sol
+++ contracts/linkStaking/OperatorVault.sol
@@ -5,17 +5,20 @@ import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import "./base/Vault.sol";
 import "./interfaces/IOperatorVCS.sol";
+import "./interfaces/IOperatorStaking.sol";
+import "./interfaces/IPFAlertsController.sol";
 
 /**
  * @title Operator Vault
- * @notice Vault contract for depositing LINK collateral into the Chainlink staking controller as an operator -
- * each vault represent a single operator
+ * @notice Vault contract for depositing LINK collateral into the Chainlink operator staking controller -
+ * each vault represents a single operator
  */
 contract OperatorVault is Vault {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     address public operator;
     address public rewardsReceiver;
+    IPFAlertsController public pfAlertsController;
 
     uint128 public trackedTotalDeposits;
     uint128 private unclaimedRewards;
@@ -37,7 +40,9 @@ contract OperatorVault is Vault {
      * @notice initializes contract
      * @param _token address of LINK token
      * @param _vaultController address of the strategy that controls this vault
-     * @param _stakeController address of Chainlink staking contract
+     * @param _stakeController address of Chainlink operator staking contract
+     * @param _rewardsController address of Chainlink staking rewards contract
+     * @param _pfAlertsController address of Chainlink price feed alrts controller
      * @param _operator address of operator represented by this vault
      * @param _rewardsReceiver address authorized to claim rewards from this vault
      **/
@@ -45,15 +50,21 @@ contract OperatorVault is Vault {
         address _token,
         address _vaultController,
         address _stakeController,
+        address _rewardsController,
+        address _pfAlertsController,
         address _operator,
         address _rewardsReceiver
     ) public reinitializer(3) {
         if (operator == address(0)) {
-            __Vault_init(_token, _vaultController, _stakeController);
+            __Vault_init(_token, _vaultController, _stakeController, _rewardsController);
             setOperator(_operator);
         } else {
+            stakeController.migrate("");
+            stakeController = IStaking(_stakeController);
             trackedTotalDeposits = SafeCast.toUint128(getTotalDeposits());
+            rewardsController = IStakingRewards(_rewardsController);
         }
+        pfAlertsController = IPFAlertsController(_pfAlertsController);
         rewardsReceiver = _rewardsReceiver;
     }
 
@@ -85,25 +96,22 @@ contract OperatorVault is Vault {
     }
 
     /**
-     * @notice returns the total balance of this contract in the Chainlink staking contract
-     * @dev includes principal plus any rewards
-     * @return total balance
+     * @notice returns the principal balance of this contract in the Chainlink staking contract
+     * @return principal balance
      */
-    function getTotalDeposits() public view override returns (uint256) {
-        return
-            stakeController.getStake(address(this)) +
-            stakeController.getBaseReward(address(this)) +
-            stakeController.getDelegationReward(address(this));
+    function getPrincipalDeposits() public view override returns (uint256) {
+        return super.getPrincipalDeposits() + IOperatorStaking(address(stakeController)).getRemovedPrincipal(address(this));
     }
 
     /**
      * @notice raises an alert in the Chainlink staking contract
+     * @param _feed address of Chainlink feed to raise alert for
      * @dev reverts if sender is not operator
      */
-    function raiseAlert() external onlyOperator {
+    function raiseAlert(address _feed) external onlyOperator {
         uint256 prevBalance = token.balanceOf(address(this));
 
-        stakeController.raiseAlert();
+        pfAlertsController.raiseAlert(_feed);
 
         uint256 rewards = token.balanceOf(address(this)) - prevBalance;
         uint256 opRewards = (rewards * IOperatorVCS(vaultController).operatorRewardPercentage()) / 10000;
@@ -156,18 +164,31 @@ contract OperatorVault is Vault {
     /**
      * @notice updates the deposit and reward accounting for this vault
      * @dev will only pay out rewards if the vault is net positive when accounting for lost deposits
+     * @param _minRewards min amount of rewards to claim (set 0 to skip reward claiming)
+     * @param _rewardsReceiver address to receive claimed rewards (set if _minRewards > 0)
      * @return totalDeposits the current total deposits in this vault
      * @return rewards the rewards earned by this vault since the last update
      */
-    function updateDeposits() external onlyVaultController returns (uint256, uint256) {
-        uint256 totalDeposits = getTotalDeposits();
+    function updateDeposits(uint256 _minRewards, address _rewardsReceiver)
+        external
+        onlyVaultController
+        returns (uint256, uint256)
+    {
+        uint256 principal = getPrincipalDeposits();
+        uint256 rewards = getRewards();
+        uint256 totalDeposits = principal + rewards;
         int256 depositChange = int256(totalDeposits) - int256(uint256(trackedTotalDeposits));
 
+        if (_minRewards != 0 && rewards >= _minRewards) {
+            rewardsController.claimReward();
+            token.safeTransfer(_rewardsReceiver, rewards);
+        }
+
         if (depositChange > 0) {
-            uint256 rewards = (uint256(depositChange) * IOperatorVCS(vaultController).operatorRewardPercentage()) / 10000;
-            unclaimedRewards += SafeCast.toUint128(rewards);
+            uint256 opRewards = (uint256(depositChange) * IOperatorVCS(vaultController).operatorRewardPercentage()) / 10000;
+            unclaimedRewards += SafeCast.toUint128(opRewards);
             trackedTotalDeposits = SafeCast.toUint128(totalDeposits);
-            return (totalDeposits, rewards);
+            return (totalDeposits, opRewards);
         }
 
         return (totalDeposits, 0);
