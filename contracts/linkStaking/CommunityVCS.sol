551a7dcda62d2b0f518f428d8f9e4ec8d23f3856
BkChoy <christian.agnew@outlook.com>
2023-09-11 19:33:46 -0400

    updated comments

--- contracts/linkStaking/CommunityVCS.sol
+++ contracts/linkStaking/CommunityVCS.sol
@@ -21,6 +21,18 @@ contract CommunityVCS is VaultControllerStrategy {
         _disableInitializers();
     }
 
+    /**
+     * @notice initializes contract
+     * @param _token address of LINK token
+     * @param _stakingPool address of the staking pool that controls this strategy
+     * @param _stakeController address of Chainlink staking contract
+     * @param _vaultImplementation address of the implementation contract to use when deploying new vaults
+     * @param _fees list of fees to be paid on rewards
+     * @param _maxDepositSizeBP basis point amount of the remaing deposit room in the Chainlink staking contract
+     * that can be deposited at once
+     * @param _vaultDeploymentThreshold the min number of non-full vaults before a new batch is deployed
+     * @param _vaultDeploymentAmount amount of vaults to deploy when threshold is met
+     **/
     function initialize(
         address _token,
         address _stakingPool,
@@ -61,13 +73,17 @@ contract CommunityVCS is VaultControllerStrategy {
         }
     }
 
+    /**
+     * @notice returns whether a new batch of vaults should be deployed
+     * @dev used by chainlink keepers
+     */
     function checkUpkeep(bytes calldata) external view returns (bool, bytes memory) {
         return ((vaults.length - 1 - indexOfLastFullVault) < vaultDeploymentThreshold, bytes(""));
     }
 
     /**
      * @notice deploys a new batch of vaults
-     * @dev will revert if the # of non-full vaults is not less than vaultDeploymentThreshold
+     * @dev will revert if the number of non-full vaults is not less than vaultDeploymentThreshold
      * @dev used by chainlink keepers
      */
     function performUpkeep(bytes calldata) external {
@@ -75,13 +91,17 @@ contract CommunityVCS is VaultControllerStrategy {
         _deployVaults(vaultDeploymentAmount);
     }
 
+    /**
+     * @notice deploys a new batch of vaults
+     * @param _numVaults number of vaults to deploy
+     */
     function addVaults(uint256 _numVaults) external onlyOwner {
         _deployVaults(_numVaults);
     }
 
     /**
      * @notice sets the vault deployment parameters
-     * @param _vaultDeploymentThreshold the min # of non-full vaults before a new batch is deployed
+     * @param _vaultDeploymentThreshold the min number of non-full vaults before a new batch is deployed
      * @param _vaultDeploymentAmount amount of vaults to deploy when threshold is met
      */
     function setVaultDeploymentParams(uint128 _vaultDeploymentThreshold, uint128 _vaultDeploymentAmount) external onlyOwner {
