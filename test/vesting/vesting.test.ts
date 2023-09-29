531384696a8d665a23c37dec372298b5d0d5aad3
BkChoy <christian.agnew@outlook.com>
2023-09-29 12:14:33 -0400

    update vesting tests

--- test/vesting/vesting.test.ts
+++ test/vesting/vesting.test.ts
@@ -1,10 +1,12 @@
 import { toEther, deploy, getAccounts, fromEther } from '../utils/helpers'
 import { StakingAllowance, Vesting } from '../../typechain-types'
 import { assert } from 'chai'
+import { time } from '@nomicfoundation/hardhat-network-helpers'
 
 describe('Vesting', () => {
   let sdlToken: StakingAllowance
   let vesting: Vesting
+  let start: number
   let accounts: string[]
 
   before(async () => {
@@ -14,12 +16,8 @@ describe('Vesting', () => {
   beforeEach(async () => {
     sdlToken = (await deploy('StakingAllowance', ['Stake Dot Link', 'SDL'])) as StakingAllowance
 
-    vesting = (await deploy('Vesting', [
-      accounts[0],
-      accounts[1],
-      1695312000,
-      86400 * 10,
-    ])) as Vesting
+    start = Math.trunc(Date.now() / 1000)
+    vesting = (await deploy('Vesting', [accounts[0], accounts[1], start, 86400 * 10])) as Vesting
 
     await sdlToken.mint(accounts[0], toEther(1000))
     await sdlToken.transfer(vesting.address, toEther(1000))
@@ -28,17 +26,17 @@ describe('Vesting', () => {
   it('vars should be correctly set', async () => {
     assert.equal(await vesting.owner(), accounts[0])
     assert.equal(await vesting.beneficiary(), accounts[1])
-    assert.equal((await vesting.start()).toNumber(), 1695312000)
+    assert.equal((await vesting.start()).toNumber(), start)
     assert.equal((await vesting.duration()).toNumber(), 86400 * 10)
   })
 
   it('should be able to terminate vesting', async () => {
+    await time.setNextBlockTimestamp(start + 4 * 86400)
     await vesting.terminateVesting([sdlToken.address])
     await vesting.releaseRemaining(sdlToken.address)
-    let ownerBalance = await sdlToken.balanceOf(accounts[0])
-    let beneficiaryBalance = await sdlToken.balanceOf(accounts[1])
-    assert.equal(ownerBalance.lt(beneficiaryBalance), true)
-    assert.equal(ownerBalance.gt(0), true)
+
+    assert.equal(fromEther(await sdlToken.balanceOf(accounts[0])), 600)
+    assert.equal(fromEther(await sdlToken.balanceOf(accounts[1])), 400)
     assert.equal(fromEther(await sdlToken.balanceOf(vesting.address)), 0)
   })
 })
