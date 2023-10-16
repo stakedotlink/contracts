1e651349e7bcb44a8913e0284bac6cc441f7b0dc
BkChoy <christian.agnew@outlook.com>
2023-10-16 13:35:15 -0400

    updated chainlink packages

--- contracts/core/SlashingKeeper.sol
+++ contracts/core/SlashingKeeper.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
 
-import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
-
 import "./interfaces/IStakingPool.sol";
 import "./interfaces/IStrategy.sol";
 
@@ -10,7 +8,7 @@ import "./interfaces/IStrategy.sol";
  * @title Slashing Keeper
  * @notice Updates strategy rewards if any losses have been incurred
  */
-contract SlashingKeeper is KeeperCompatibleInterface {
+contract SlashingKeeper {
     IStakingPool public stakingPool;
 
     constructor(address _stakingPool) {
@@ -22,7 +20,7 @@ contract SlashingKeeper is KeeperCompatibleInterface {
      * @return upkeepNeeded whether or not rewards should be updated
      * @return performData abi encoded list of strategy indexes to update
      **/
-    function checkUpkeep(bytes calldata) external view override returns (bool, bytes memory) {
+    function checkUpkeep(bytes calldata) external view returns (bool, bytes memory) {
         address[] memory strategies = stakingPool.getStrategies();
         bool[] memory strategiesToUpdate = new bool[](strategies.length);
         uint256 totalStrategiesToUpdate;
@@ -56,7 +54,7 @@ contract SlashingKeeper is KeeperCompatibleInterface {
      * @notice Updates rewards
      * @param _performData abi encoded list of strategy indexes to update
      */
-    function performUpkeep(bytes calldata _performData) external override {
+    function performUpkeep(bytes calldata _performData) external {
         address[] memory strategies = stakingPool.getStrategies();
         uint256[] memory strategiesToUpdate = abi.decode(_performData, (uint256[]));
         require(strategiesToUpdate.length > 0, "No strategies to update");
