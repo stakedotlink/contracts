1b40103f76b39f339494ffb46337b684dad491a8
BkChoy <christian.agnew@outlook.com>
2024-04-25 12:59:50 -0400

    overhauled test deployment scripts

---
+++ scripts/test/deploy/deploy.ts
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import { deployCore } from './modules/deploy-core'
+import { deployLINKStaking } from './modules/deploy-link-staking'
+import { deployTestContracts } from './modules/deploy-test-contracts'
+
+const path = './deployments/localhost.json'
+
+async function main() {
+  if (fs.existsSync(path)) {
+    fs.unlinkSync(path)
+  }
+
+  await deployTestContracts()
+  await deployCore()
+  await deployLINKStaking()
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
