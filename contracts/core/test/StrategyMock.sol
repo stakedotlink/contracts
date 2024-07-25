81ee92d2adee23a733a76073acdbb64713a1e2c0
BkChoy <christian.agnew@outlook.com>
2024-07-25 09:25:59 -0400

    organized rewards pools

--- contracts/core/test/StrategyMock.sol
+++ contracts/core/test/StrategyMock.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 import "../base/Strategy.sol";
-import "../RewardsPool.sol";
+import "../rewardsPools/RewardsPool.sol";
 
 /**
  * @title Strategy Mock
