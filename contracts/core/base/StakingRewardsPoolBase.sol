aca11b43b1df29c29d4903269eedb0618685e520
BkChoy <christian.agnew@outlook.com>
2024-07-29 09:05:04 -0400

    support multiple rewards pools in insurance pool

--- contracts/core/base/StakingRewardsPoolBase.sol
+++ contracts/core/base/StakingRewardsPoolBase.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-
 import "../tokens/base/ERC677Upgradeable.sol";
 
 /**
- * @title Staking Rewards Pool
- * @notice Handles staking and reward distribution for a single asset
- * @dev Rewards can be positive or negative (user balances can increase and decrease)
+ * @title Staking Rewards Pool Base
+ * @dev Staking Rewards Pool without UUPSUpgradeable and OwnableUpgradeable
  */
-abstract contract StakingRewardsPool is ERC677Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
+abstract contract StakingRewardsPoolBase is ERC677Upgradeable {
     uint256 private constant DEAD_SHARES = 10 ** 3;
 
     IERC20Upgradeable public token;
@@ -19,14 +15,12 @@ abstract contract StakingRewardsPool is ERC677Upgradeable, UUPSUpgradeable, Owna
     mapping(address => uint256) private shares;
     uint256 public totalShares;
 
-    function __StakingRewardsPool_init(
+    function __StakingRewardsPoolBase_init(
         address _token,
         string memory _derivativeTokenName,
         string memory _derivativeTokenSymbol
     ) public onlyInitializing {
         __ERC677_init(_derivativeTokenName, _derivativeTokenSymbol, 0);
-        __UUPSUpgradeable_init();
-        __Ownable_init();
         token = IERC20Upgradeable(_token);
     }
 
@@ -205,6 +199,4 @@ abstract contract StakingRewardsPool is ERC677Upgradeable, UUPSUpgradeable, Owna
 
         emit Transfer(_account, address(0), _amount);
     }
-
-    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
