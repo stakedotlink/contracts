85c115a27c13ee686a8693c1e24edc44f932edb6
1marcghannam <1marc.ghannam@gmail.com>
2024-09-04 10:13:11 -0400

    feat: remove duplicate contracts

--- scripts/test/deploy/modules/deploy-subgraph-mocks.ts
+++ scripts/test/deploy/modules/deploy-subgraph-mocks.ts
@@ -2,31 +2,31 @@ import { deploy } from '../../../utils/deployment'
 
 export async function deploySubgraphMockContracts() {
   const mockEthUsdAggregator = await deploy(
-    'contracts/core/test/EthUsdAggregator.sol:MockEthUsdAggregator',
+    'contracts/core/test/subgraphMocks/EthUsdAggregator.sol:MockEthUsdAggregator',
     []
   )
   console.log('MockEthUsdAggregator deployed: ', mockEthUsdAggregator.target)
 
   const mockLinkSdlSushiPool = await deploy(
-    'contracts/core/test/LinkSdlSushiPool.sol:MockLinkSdlSushiPool',
+    'contracts/core/test/subgraphMocks/LinkSdlSushiPool.sol:MockLinkSdlSushiPool',
     []
   )
   console.log('MockLinkSdlSushiPool deployed: ', mockLinkSdlSushiPool.target)
 
   const mockLinkSdlUniswapPool = await deploy(
-    'contracts/core/test/LinkSdlUniswapPool.sol:MockLinkSdlUniswapPool',
+    'contracts/core/test/subgraphMocks/LinkSdlUniswapPool.sol:MockLinkSdlUniswapPool',
     []
   )
   console.log('MockLinkSdlUniswapPool deployed: ', mockLinkSdlUniswapPool.target)
 
   const mockLinkUsdAggregator = await deploy(
-    'contracts/core/test/LinkUsdAggregator.sol:MockLinkUsdAggregator',
+    'contracts/core/test/subgraphMocks/LinkUsdAggregator.sol:MockLinkUsdAggregator',
     []
   )
   console.log('MockLinkUsdAggregator deployed: ', mockLinkUsdAggregator.target)
 
   const mockMetisEthUniswapPool = await deploy(
-    'contracts/core/test/MetisEthUniswapPool.sol:MockMetisEthUniswapPool',
+    'contracts/core/test/subgraphMocks/MetisEthUniswapPool.sol:MockMetisEthUniswapPool',
     []
   )
   console.log('MockMetisEthUniswapPool deployed: ', mockMetisEthUniswapPool.target)
