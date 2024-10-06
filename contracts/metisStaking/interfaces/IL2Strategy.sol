c938a33cbac5faa2ee1ce83b0825cfef7c79ee5e
BkChoy <christian.agnew@outlook.com>
2024-10-06 17:19:59 -0400

    Added Metis L2 strategy

---
+++ contracts/metisStaking/interfaces/IL2Strategy.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+interface IL2Strategy {
+    function handleIncomingTokensFromL1(uint256 _amount) external;
+
+    function handleOutgoingTokensToL1(uint256 _amount) external;
+
+    function tokensInTransitFromL1() external view returns (uint256);
+
+    function getTotalQueued() external view returns (uint256);
+
+    function handleUpdateFromL1(
+        uint256 _totalDeposits,
+        uint256 _tokensInTransitFromL1,
+        uint256 _tokensReceivedAtL1,
+        address[] calldata _opRewardReceivers,
+        uint256[] calldata _opRewardAmounts
+    ) external;
+}
