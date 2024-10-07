6e55c54b9643ea161bf89edc7ca62aa5ea4b7e30
BkChoy <christian.agnew@outlook.com>
2024-10-07 13:55:31 -0400

    Added Metis L2 transmitter

---
+++ contracts/metisStaking/interfaces/IL2StandardBridge.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+interface IL2StandardBridge {
+    function withdrawMetisTo(
+        address _to,
+        uint256 _amount,
+        uint32 _l1Gas,
+        bytes calldata _data
+    ) external payable;
+}
