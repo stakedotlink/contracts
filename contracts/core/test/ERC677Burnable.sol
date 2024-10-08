0823925c599e1ceb251baca49547ab9729d9c2d7
BkChoy <christian.agnew@outlook.com>
2024-10-07 22:06:39 -0400

    Metis L2 transmitter tests

---
+++ contracts/core/test/ERC677Burnable.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+import "../tokens/base/ERC677.sol";
+
+contract ERC677Burnable is ERC677 {
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256 _totalSupply
+    ) ERC677(_tokenName, _tokenSymbol, _totalSupply) {}
+
+    function burn(address _from, uint256 _amount) external {
+        _burn(_from, _amount);
+    }
+}
