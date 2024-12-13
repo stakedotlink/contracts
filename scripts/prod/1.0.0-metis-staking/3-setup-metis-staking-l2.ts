15e2a936059725d6e56886797216aefdcb7a72fe
BkChoy <christian.agnew@outlook.com>
2024-12-13 08:36:56 -0500

    added metis rebase controller deployment scripts

--- scripts/prod/1.0.0-metis-staking/3-setup-metis-staking-l2.ts
+++ scripts/prod/1.0.0-metis-staking/3-setup-metis-staking-l2.ts
@@ -1,12 +1,9 @@
 import { L2Transmitter } from '../../../typechain-types/contracts/metisStaking/L2Transmitter'
 import { getContract } from '../../utils/deployment'
-import { switchNetwork } from '../../utils/helpers'
 
 const l1Transmitter = ''
 
 async function main() {
-  await switchNetwork(1088, '')
-
   const l2Transmitter = (await getContract('METIS_L2Transmitter', true)) as L2Transmitter
 
   await (await l2Transmitter.setL1Transmitter(l1Transmitter)).wait()
